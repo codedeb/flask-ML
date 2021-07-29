d13f7fc22307053dd68ff23e3036789f9f00de48
223019194 <ram.kothapalli@ge.com>
2021-07-29 16:28:49 +0530

    health check api

--- ocr_wrapper_service/api_1_0/api.py
+++ ocr_wrapper_service/api_1_0/api.py
@@ -3,6 +3,7 @@ from flask_restplus import Api, fields
 from .messages_api import api as messages_ns
 from .analytic_api import api as analytic_ns
 from .images_api import api as image_ns
+from .healthcheck_api import api as status_ns
 
 blueprint = Blueprint("api", __name__, url_prefix="")
 api = Api(app=blueprint,
@@ -19,4 +20,5 @@ input_fields_res = api.inherit("input", input_fields)
 api.add_namespace(messages_ns, path='/messages-ns')
 api.add_namespace(analytic_ns, path='/analytic-ns')
 api.add_namespace(image_ns, path='/image-ns')
+api.add_namespace(status_ns, path='/api')
 # from .views import api
