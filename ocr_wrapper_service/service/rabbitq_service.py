b52588e9526500c0534c1fd8a5c467cc6fbb1449
Sharma <212602827@ge.com>
2021-09-09 15:50:39 +0530

    exception logs for output queue

--- ocr_wrapper_service/service/rabbitq_service.py
+++ ocr_wrapper_service/service/rabbitq_service.py
@@ -12,27 +12,32 @@ logger = logging.getLogger(__name__)
 
 
 def send_messages(output):
-    logger.info('in send')
-    hostname = os.environ['RABBITMQ_HOST_NAME']
-    port = os.environ['RABBITMQ_HOST_PORT']
-    username = os.environ['RABBITMQ_USERNAME']
-    password = os.environ['RABBITMQ_PASSWORD']
-    output_queue = os.environ['RABBITMQ_OUTPUT_QUEUE']
-    exchange = os.environ['RABBITMQ_EXCHANGE']
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port,
-                                                                   credentials=pika.credentials.PlainCredentials(
-                                                                       username, password)))
-    channel = connection.channel()
+    try:
 
-    channel.queue_declare(queue=output_queue, durable=True)
+        logger.info('in send')
+        hostname = os.environ['RABBITMQ_HOST_NAME']
+        port = os.environ['RABBITMQ_HOST_PORT']
+        username = os.environ['RABBITMQ_USERNAME']
+        password = os.environ['RABBITMQ_PASSWORD']
+        output_queue = os.environ['RABBITMQ_OUTPUT_QUEUE']
+        exchange = os.environ['RABBITMQ_EXCHANGE']
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port,
+                                                                    credentials=pika.credentials.PlainCredentials(
+                                                                        username, password)))
+        channel = connection.channel()
 
-    channel.basic_publish(exchange=exchange, routing_key=output_queue, body=json.dumps(output),
-                          properties=pika.BasicProperties(
-                              delivery_mode=2,  # make message persistent
-                          ))
-    logger.info(" [x] Sent Output to Queue")
-    connection.close()
+        channel.queue_declare(queue=output_queue, durable=True)
+
+        channel.basic_publish(exchange=exchange, routing_key=output_queue, body=json.dumps(output),
+                            properties=pika.BasicProperties(
+                                delivery_mode=2,  # make message persistent
+                            ))
+        logger.info(" [x] Sent Output to Queue")
+        connection.close()
+    except Exception as e:
+        logger.error('Failed in queue output : %s' % e)
     return True
+    
 
 
 def process_messages():
@@ -67,10 +72,10 @@ def process_messages():
         channel.start_consuming()
 
     except pika.exceptions.ConnectionClosedByBroker:
-        logger.info('Rabbitmq connection closed by broker!')
+        logger.error('Rabbitmq connection closed by broker!')
     # Don't recover on channel errors
     except pika.exceptions.AMQPChannelError:
-        logger.info('Rabbitmq channel error!')
+        logger.error('Rabbitmq channel error!')
     # Recover on all other connection errors
     except pika.exceptions.AMQPConnectionError:
-        logger.info('Rabbitmq connection error!')
+        logger.error('Rabbitmq connection error!')
