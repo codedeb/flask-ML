afddd6578cfda06465521eeae806fe94a2c52f40
Sharma <212602827@ge.com>
2021-09-17 19:41:03 +0530

    Checking connection for queue

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 # Using base image as slim-buster as it is 114MB when uncompressed with latest Python releases and benefits of Debian Buster
 FROM python:3.6-slim-buster
 
-ENV HTTPS_PROXY "http://PITC-Zscaler-Americas-Alpharetta3pr.proxy.corporate.ge.com:80"
-ENV HTTP_PROXY "http://PITC-Zscaler-Americas-Alpharetta3pr.proxy.corporate.ge.com:80"
+# ENV HTTPS_PROXY "http://PITC-Zscaler-Americas-Alpharetta3pr.proxy.corporate.ge.com:80"
+# ENV HTTP_PROXY "http://PITC-Zscaler-Americas-Alpharetta3pr.proxy.corporate.ge.com:80"
 
-COPY config/80proxy /etc/apt/apt.conf.d/80proxy
+# COPY config/80proxy /etc/apt/apt.conf.d/80proxy
 
 RUN apt-get update -y \
     && apt-get clean \
@@ -35,7 +35,7 @@ RUN pip3 install -U detectron2/.
 
 # Copy the code from local to docker contanier
 COPY . /ocr-wrapper-service
-WORKDIR ocr-wrapper-service
+WORKDIR /ocr-wrapper-service
 
 EXPOSE 5000
 
--- ocr_wrapper_service/service/rabbitq_service.py
+++ ocr_wrapper_service/service/rabbitq_service.py
@@ -27,7 +27,6 @@ def send_messages(output):
 
         channel.queue_declare(queue=output_queue, durable=True)
         logger.info("Ouptut: %s" % output)
-        logger.info("Ouptut json dump: %s" % json.dumps(output))
 
         channel.basic_publish(exchange=exchange, routing_key=output_queue, body=json.dumps(output),
                             properties=pika.BasicProperties(
@@ -37,46 +36,63 @@ def send_messages(output):
         connection.close()
     except Exception as e:
         logger.info("Exceptions for output queue: %s" % e)
+        connection.close()
         pass
     return True
     
 
+class abc():
+    def __init__(self):
+        # self.process_messages()
+        logger.info("Testing abc")
+       
+        hostname = os.environ['RABBITMQ_HOST_NAME']
+        port = os.environ['RABBITMQ_HOST_PORT']
+        username = os.environ['RABBITMQ_USERNAME']
+        password = os.environ['RABBITMQ_PASSWORD']
+        input_queue = os.environ['RABBITMQ_INPUT_QUEUE']
+        self.queue_connect = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port,
+                                                                        credentials=pika.credentials.PlainCredentials(
+                                                                            username, password)))
 
-def process_messages():
-    hostname = os.environ['RABBITMQ_HOST_NAME']
-    port = os.environ['RABBITMQ_HOST_PORT']
-    username = os.environ['RABBITMQ_USERNAME']
-    password = os.environ['RABBITMQ_PASSWORD']
-    input_queue = os.environ['RABBITMQ_INPUT_QUEUE']
-    # try:
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port,
-                                                                credentials=pika.credentials.PlainCredentials(
-                                                                    username, password)))
-    logger.info('Connected to rabbitmq successfully!')
-    channel = connection.channel()
-    channel.queue_declare(queue=input_queue, durable=True)
-    logger.info(' [*] Waiting for messages.')
+        logger.info("Calling process messages")
+        self.process_messages()
+                                                        
+            
+    def process_messages(self):
+        logger.info("Inside process messages")
+        hostname = os.environ['RABBITMQ_HOST_NAME']
+        port = os.environ['RABBITMQ_HOST_PORT']
+        username = os.environ['RABBITMQ_USERNAME']
+        password = os.environ['RABBITMQ_PASSWORD']
+        input_queue = os.environ['RABBITMQ_INPUT_QUEUE']
+        try:
+            logger.info("Checking Connection: ")
+            self.queue_connect.is_open
+            logger.info("Connection exists!")
+            channel = self.queue_connect.channel()
+        except Exception as e:
+            logger.info("Connection not present!")
+            self.queue_connect = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port,
+                                                                        credentials=pika.credentials.PlainCredentials(
+                                                                            username, password)))
+            logger.info('Connected to rabbitmq successfully!')
+            channel = self.queue_connect.channel()
+            
+        
+        channel.queue_declare(queue=self.input_queue, durable=True)
+        logger.info(' [*] Waiting for messages.')
 
-    def callback(ch, method, properties, body):
-        logger.info(" [x] Received %r" % body)
-        input_messages = body.decode('utf8')
-        print(input_messages)
-        output_messages = process(input_messages)
-        # if len(output_messages) > 0:
-        #     logger.info('length > 0')
-        send_messages(output_messages)
+        def callback(ch, method, properties, body):
+            logger.info(" [x] Received %r" % body)
+            input_messages = body.decode('utf8')
+            print(input_messages)
+            output_messages = process(input_messages)
+            # if len(output_messages) > 0:
+            #     logger.info('length > 0')
+            send_messages(output_messages)
 
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue=input_queue, on_message_callback=callback, auto_ack=True)
-    channel.start_consuming()
-    # except pika.exceptions.ConnectionClosedByBroker:
-    #     logger.error('Rabbitmq connection closed by broker!')
-    #     continue
-    # # Don't recover on channel errors
-    # except pika.exceptions.AMQPChannelError as err:
-    #     logger.error('Caught a channel error: {}, stopping... %s ' % err)
-    #     continue
-    # # Recover on all other connection errors
-    # except pika.exceptions.AMQPConnectionError:
-    #     logger.error('Connection was closed, retrying...')
-    #     continue
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(queue=input_queue, on_message_callback=callback, auto_ack=True)
+        channel.start_consuming()
+   
