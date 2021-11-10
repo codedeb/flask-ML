784bc04fde7af5e074cc8405280521b5239bdf2d
Sharma <212602827@ge.com>
2021-11-10 19:37:28 +0530

    Updated models for recovery prefix for aws

--- ocr_analytic_service/service/dot_punch_mod.py
+++ ocr_analytic_service/service/dot_punch_mod.py
@@ -12,10 +12,12 @@ logger = logging.getLogger(__name__)
 
 
 def dot_punched_data_parser(imgobj):
+    # config_path = "ocr_analytic_service/service/configDotPunch_file.yaml"
     config_path = "ocr_analytic_service/service/configDotPunch_file_psn.yaml"
 
-    base_path = os.getenv('MODEL_PATH')
-    model_weight_path = os.path.join(base_path, "model/model_final_dotpunch.pth")
+    base_path = os.getenv("NAS_PATH")
+    model_weight_path = os.path.join(base_path, "models/model_final_dotpunch.pth")
+    # model_weight_path = r"/shared-volume/model_final_dotpunch.pth"
     logger.info('Dot punch model path: %s' % model_weight_path)
 
     threshold = 0.8
@@ -28,6 +30,6 @@ def dot_punched_data_parser(imgobj):
         out_obj['ocrValue'] = str_ocr
         out_obj['confValue'] = conf
         out_obj['confBand'] = conf_band
-    except exception as e:
+    except Exception as e:
         logger.info('no data found: %s' % e)
     return out_obj
