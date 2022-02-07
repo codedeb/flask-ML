209ef8bb954b9329dd8940da3ba85cfabc9ba62d
Kumar <223048697@ge.com>
2022-02-07 11:00:05 +0530

    tidy up

--- ocr_wrapper_service/api_1_1/health_status.py
+++ ocr_wrapper_service/api_1_1/health_status.py
@@ -1,11 +1,5 @@
-import json
-from flask import jsonify, request
 from flask import Response
-#from ocr_wrapper_service.service.analytic_service import process_images
-
-#api = Namespace(name='analytic-ns', description='Analytic Operations')
-
-from flask import Blueprint, render_template
+from flask import Blueprint
 
 health_blueprint = Blueprint('health', __name__)
 
@@ -13,4 +7,3 @@ health_blueprint = Blueprint('health', __name__)
 def health_status():
     result = {'status': 'UP'}
     return result
-
