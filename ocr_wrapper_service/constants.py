cb85a6aa75254d6166ceda299dc653a05b66147f
debashish <Debashish.Chakraborty@ge.com>
2022-05-02 12:03:10 +0530

    added configuration file for dev

--- ocr_wrapper_service/constants.py
+++ ocr_wrapper_service/constants.py
@@ -1,4 +1,11 @@
+from configparser import ConfigParser
 import os
+
+filename = 'ocr_wrapper_service/config.ini'
+config = ConfigParser()
+config.read(filename)
+
+
 class LoggerConstants():
     console_format="[%(levelname)s] %(asctime)s %(filename)s:%(lineno)d %(message)s"
     date_format="%Y-%m-%d %H:%M:%S"
@@ -25,37 +32,47 @@ class S3Constants():
     bucket_name=os.getenv("BUCKET_NAME")
     region=os.getenv("REGION")
     max_keys=10
-    model_path="IDM/model/ocr_model_shrouds_v1.0.0/model"
-    model_count=5
-    model_names=["^model_dotpunch_v.*.pth$","^model_prefix_v.*.pth$","^model_segmentation_v.*.pth$","^model_shroud_segmentation_v.*.pth$","^model_shroud_OCR_v.*.pth$"]
+    # model_path="IDM/model/ocr_model_psn_v1.0.0/model"
+    model_path = "IDM/model/ocr_model_shrouds_v1.0.0/"
+    model_count=3
+    model_names=["^model_dotpunch_v.*.pth$","^model_prefix_v.*.pth$","^model_segmentation_v.*.pth$"]
     retry_sleep=300
 
 class LocalDirectoryConstants():
-    model_path=os.getenv('CONTAINER_MODEL_PATH')+"/model/"
-    # model_path="/models"
+    # model_path=os.getenv('CONTAINER_MODEL_PATH')+"/model/"
+    # # model_path="/models"
+    model_path = f"{config['S3']['MODEL_PATH']}{config['DEFAULT']['ACTIVE_RELEASE']}/"
 
 class ModelDetails():
+    active_release = config['DEFAULT']['ACTIVE_RELEASE']
+    active_version = config[active_release]
+    container_path = f"{config['S3']['MODEL_PATH']}{active_release}/"
     blade_config_base_path="ocr_analytic_service/componentBlade/"
-    model_base_path = os.getenv('CONTAINER_MODEL_PATH')+"/model/"
-    # model_base_path = "/shared-volume/shroud_models/"
-    segmentation_config_path=blade_config_base_path+"configSeg.yaml"
-    segmentation_model_path=model_base_path+"model_segmentation_v1.1.0.pth"
-    segmentation_threshold=0.3
-    dot_punch_pickle_path = blade_config_base_path + "listPickle"
-    dot_punch_config_path = blade_config_base_path + "configDotPunchPSN.yaml"
-    dot_punch_model_path = model_base_path + "model_dotpunch_v1.1.0.pth"
-    dot_punch_threshold=0.8
-    prefix_config_path = blade_config_base_path + "configPrefix.yaml"
-    prefix_model_path = model_base_path + "model_prefix_v1.1.0.pth"
-    prefix_threshold=0.1
+
+    segmentation_config_path=blade_config_base_path + active_version['SHROUD_SEG_CONFIG']
+    segmentation_model_path=container_path + active_version["BLADE_SEG_MODEL"]
+    segmentation_threshold=active_version['BLADE_SEG_THRESHOLD']
+    
+    
+    dot_punch_config_path = blade_config_base_path + active_version['BLADE_PSN_CONFIG']
+    dot_punch_model_path = container_path + active_version['BLADE_PSN_MODEL']
+    dot_punch_threshold=active_version['BLADE_PSN_THRESHOLD']
+    
+    prefix_config_path = blade_config_base_path + active_version['BLADE_PREFIX_CONFIG']
+    prefix_model_path = container_path + active_version['BLADE_PREFIX_MODEL']
+    prefix_threshold=active_version['BLADE_PREFIX_THRESHOLD']
+    
+    dot_punch_pickle_path = blade_config_base_path + active_version['BLADE_PICKLE']
 
     shroud_config_base_path = "ocr_analytic_service/componentShroud/"
-    shroud_seg_config_path = shroud_config_base_path + "config_shroud_segmentation_v3.yaml"
-    shroud_seg_model_path = model_base_path + "model_shroud_segmentation_v3.pth"
-    shroud_seg_threshold = 0.1
-    shroud_ocr_config_path = shroud_config_base_path + "config_shroud_OCR_v6.yaml"
-    shroud_ocr_model_path = model_base_path + "model_shroud_OCR_v6.pth"
-    shroud_ocr_threshold = 0.3
+    
+    shroud_seg_config_path = shroud_config_base_path + active_version['SHROUD_SEG_CONFIG']
+    shroud_seg_model_path = container_path + active_version['SHROUD_SEG_MODEL']
+    shroud_seg_threshold = float(active_version['SHROUD_SEG_THRESHOLD'])
+    
+    shroud_ocr_config_path = shroud_config_base_path + active_version['SHROUD_OCR_CONFIG']
+    shroud_ocr_model_path = container_path + active_version['SHROUD_OCR_MODEL']
+    shroud_ocr_threshold = float(active_version['SHROUD_OCR_THRESHOLD'])
     
 
 
