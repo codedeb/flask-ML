bae2245f0f0ac853bdc84c0682ac8374e091f9cf
debashish <Debashish.Chakraborty@ge.com>
2022-05-25 18:48:50 +0530

    increased retry and read attemps

--- wrapper_service/utils/aws_services.py
+++ wrapper_service/utils/aws_services.py
@@ -40,7 +40,9 @@ def sqs_client():
         sqs_client = boto3.client(
                                 'sqs', 
                                 region_name=SQSConstants.region,
-                                config=Config(connect_timeout=120, read_timeout=10, retries={'max_attempts': 2})
+                                config=Config(connect_timeout=120, read_timeout=120, retries={'max_attempts': 5,
+                                'mode': 'standard'}
+                                )
                                 )
     except Exception as e:
         logger.error(f"error while intializing sqs client : {e}")
