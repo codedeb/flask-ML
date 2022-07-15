047481837d84d6027af8e615fbc5826a52aad867
debashish <Debashish.Chakraborty@ge.com>
2022-07-15 15:20:18 +0530

    pytest command not found in jenkins build solved !!

--- utScript.sh
+++ utScript.sh
@@ -3,7 +3,9 @@ echo "UTSCRIPT - Run OCR Unit Tests!!!"
 
 echo "set CHROME_BIN"
 export CHROME_BIN="/bin/google-chrome"
-
 echo "starting pytest"
 
-pytest  tests/functional/analytics_tests.py
+pip3 install pytest
+python -m pytest tests/functional/analytics_tests.py
+
+echo "Testing ends!!!"
