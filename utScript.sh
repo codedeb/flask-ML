c8a77745383d2796238db085ee5ce3649e6a89a3
debashish <Debashish.Chakraborty@ge.com>
2022-07-19 18:07:26 +0530

    added Werkzeug

--- utScript.sh
+++ utScript.sh
@@ -5,7 +5,6 @@ echo "set CHROME_BIN"
 export CHROME_BIN="/bin/google-chrome"
 echo "starting pytest"
 
-pip3 install pytest
 python -m pytest tests/functional/analytics_tests.py
 
 echo "Testing ends!!!"
