5e96637afe23b9373994fcdffb8d19b308227a1b
Lanka, Satya Prasad <satyaprasad.lanka@ge.com>
2022-07-19 14:32:15 +0530

    Update utScript.sh

--- utScript.sh
+++ utScript.sh
@@ -5,7 +5,7 @@ echo "set CHROME_BIN"
 export CHROME_BIN="/bin/google-chrome"
 echo "starting pytest"
 
-pip3 install pytest
+#pip3 install pytest
 python -m pytest tests/functional/analytics_tests.py
 
 echo "Testing ends!!!"
