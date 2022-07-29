01181abcf9f38885b5a0754193960c30b3c41d91
debashish <Debashish.Chakraborty@ge.com>
2022-07-29 09:33:14 +0530

    modified build file

--- buildScript.sh
+++ buildScript.sh
@@ -3,9 +3,11 @@
 echo "BUILD SCRIPT !!! "
 echo "collecting packages"
 
-pip3 install -r requirements.txt 
+pip install -r requirements.txt 
 git clone --depth 1 --branch v0.1 https://github.com/facebookresearch/detectron2.git
-pip3 install -U detectron2/
-pip3 install "git+https://github.com/philferriere/cocoapi.git#egg=pycocotools&subdirectory=PythonAPI"
+pip install -U detectron2
+pip install "git+https://github.com/philferriere/cocoapi.git#egg=pycocotools&subdirectory=PythonAPI"
+
+
 
 echo "package collection done!!!"
