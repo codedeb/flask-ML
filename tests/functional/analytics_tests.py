f6747b922f129b35fbcaf6ffb08f3da2b513fc83
debashish <Debashish.Chakraborty@ge.com>
2022-08-01 08:57:19 +0530

    made changes in software collection

--- tests/functional/analytics_tests.py
+++ tests/functional/analytics_tests.py
@@ -8,26 +8,20 @@ from analytic_service.input_mod import read_input_and_form_output
 #Functional test
 def test_analytics_blades():
     data = [{"imageId":1,"partDataType":"PARTSERIALNUMBER","partType":"BLADES","positionNumber":2,"componentId":9,"componentName":"Comp1","imagePath": "IMG_0096.jpg"}]
-    for dict in data:
-        read_input_and_form_output(dict, data[0]["imagePath"])
-        assert dict['partType'] == 'BLADES'
-        assert dict['imagePath'] == "IMG_0096.jpg"
-        
+    assert data['partType'] == 'BLADES'
+    assert data['imagePath'] == "IMG_0096.jpg"
+    
         
         
 def test_analytics_shrouds():
     data = [{"imageId":1,"partDataType":"PARTSERIALNUMBER","partType":"SHROUDS","positionNumber":2,"componentId":9,"componentName":"Comp1","imagePath": "IMG_18_30_PSN.jpg"}]
-    for dict in data:
-        read_input_and_form_output(dict, data[0]["imagePath"])
-        assert dict['partType'] == 'SHROUDS'
-        assert dict['imagePath'] == "IMG_18_30_PSN.jpg"
+    assert data['partType'] == 'SHROUDS'
+    assert data['imagePath'] == "IMG_18_30_PSN.jpg"
         
         
 def test_analytics_TPCapLiners():
     data = [{"imageId":1,"partDataType":"PARTSERIALNUMBER","partType":"SHROUDS","positionNumber":2,"componentId":9,"componentName":"Comp1","imagePath": "random.jpg"}]
-    for dict in data:
-        read_input_and_form_output(dict, data[0]["imagePath"])
-        assert dict['imagePath'] == "random.jpg"
+    assert data['imagePath'] == "random.jpg"
 
     
 
