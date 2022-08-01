2de5c5ee75e7a9e675ab3e2e93e3df806b5d83d2
debashish <Debashish.Chakraborty@ge.com>
2022-08-01 09:31:19 +0530

    changes made for only test requirements

--- buildScript.sh
+++ buildScript.sh
@@ -8,7 +8,8 @@ echo "collecting packages"
 # /usr/local/bin/pip3 install -U detectron2/.   
 # /usr/local/bin/pip3 install "git+https://github.com/philferriere/cocoapi.git#egg=pycocotools&subdirectory=PythonAPI"
 
-/usr/local/bin/pip3 install -r requirements.txt
+
+/usr/local/bin/pip3 install -r trequirements.txt
 
 echo "package collection done!!!"
 
--- tests/functional/analytics_tests.py
+++ tests/functional/analytics_tests.py
@@ -8,20 +8,20 @@ from analytic_service.input_mod import read_input_and_form_output
 #Functional test
 def test_analytics_blades():
     data = [{"imageId":1,"partDataType":"PARTSERIALNUMBER","partType":"BLADES","positionNumber":2,"componentId":9,"componentName":"Comp1","imagePath": "IMG_0096.jpg"}]
-    assert data['partType'] == 'BLADES'
-    assert data['imagePath'] == "IMG_0096.jpg"
+    assert data[0]['partType'] == 'BLADES'
+    assert data[0]['imagePath'] == "IMG_0096.jpg"
     
         
         
 def test_analytics_shrouds():
     data = [{"imageId":1,"partDataType":"PARTSERIALNUMBER","partType":"SHROUDS","positionNumber":2,"componentId":9,"componentName":"Comp1","imagePath": "IMG_18_30_PSN.jpg"}]
-    assert data['partType'] == 'SHROUDS'
-    assert data['imagePath'] == "IMG_18_30_PSN.jpg"
+    assert data[0]['partType'] == 'SHROUDS'
+    assert data[0]['imagePath'] == "IMG_18_30_PSN.jpg"
         
         
 def test_analytics_TPCapLiners():
     data = [{"imageId":1,"partDataType":"PARTSERIALNUMBER","partType":"SHROUDS","positionNumber":2,"componentId":9,"componentName":"Comp1","imagePath": "random.jpg"}]
-    assert data['imagePath'] == "random.jpg"
+    assert data[0]['imagePath'] == "random.jpg"
 
     
 
