4d7de36e84fdcbc87aa7e88a29dad72fe53a8e82
debashish <Debashish.Chakraborty@ge.com>
2022-08-03 15:09:26 +0530

    added modification on sonarScript file

--- sonarScript.sh
+++ sonarScript.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
 echo "Run Sonar scanner" 
-sonar-scanner.sh
+
+mkdir sonarscanner -p
+
+cd /sonarscanner
+
+/usr/local/bin/pip3 install wget
+
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-linux.zip
+
+unzip sonar-scanner-cli-4.2.0.1873-linux.zip
+
+rm sonar-scanner-cli-4.2.0.1873-linux.zip
+
+chmod +x sonar-scanner-4.2.0.1873-linux/bin/sonar-scanner
+
+# ln -s sonarscanner/sonar-scanner-4.2.0.1873-linux/bin/sonar-scanner/
+
+sonar-scanner.sh start
+
 echo "Completed SonarScan for OCR"
