46ccfcee7c62bb5cc4143fe9bfade77c38de5abf
debashish <Debashish.Chakraborty@ge.com>
2022-08-09 13:40:56 +0530

    not required items

--- sonarScript.sh
+++ sonarScript.sh
@@ -2,18 +2,6 @@
 
 echo "Run Sonar scanner" 
 
-/usr/local/bin/pip3 install wget
-
-wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-linux.zip
-
-unzip sonar-scanner-cli-4.2.0.1873-linux.zip
-
-mv sonar-scanner-cli-4.2.0.1873-linux /opt/sonar/
-
-
-
-PATH=/opt/sonar/ sonar-scanner-4.2.0.1873-linux/bin
-
-sonar-scanner
+sonar-scanner.bat -v
 
 echo "Completed SonarScan for OCR"
