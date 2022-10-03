3754b66735c8bbe0a12769a1050cebf2ba420f5d
debashish <Debashish.Chakraborty@ge.com>
2022-10-03 18:16:41 +0530

    modified status up API

--- wrapper_service/api_1_1/health_status.py
+++ wrapper_service/api_1_1/health_status.py
@@ -14,23 +14,24 @@ from requests.auth import HTTPBasicAuth
 logger = logging.getLogger(__name__)
 
 health_blueprint = Blueprint('health', __name__)
-auth = HTTPBasicAuth('CPL@gepowerandwater-S7O7S9.HRELEZ', '1e532910-9b92-4902-89b5-4dfedd0caac3')
+
 @health_blueprint.route('/api/status')
 def healthy():
-    try:
-        bhoomi_url = 'https://boomi.power.ge.com/ws/rest/gegaspower/CPL/Parts/Nomination/PartsOut/parts-out-set-data/1358200'
-        data = requests.get(bhoomi_url, auth=auth)
-        if data.status_code == 200: 
-            status = 'UP'
-        else:
-            status = 'DOWN'
-        logger.info(f" OCR health Status is UP @ {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
-        logger.info(f" Bhoomi health Status is {status} @ {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
-    except:
-        status = 'DOWN'
-    
+    status = {'status' : 'UP'}
+    logger.info(f" OCR {status} @ {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
     return status
-
+    # status['OCR'] = 'UP'
+    # try:
+    #     bhoomi_url = 'https://boomi.power.ge.com/ws/rest/gegaspower/CPL/Parts/Nomination/PartsOut/parts-out-set-data/1358200'
+    #     data = requests.get(bhoomi_url, auth=auth)
+    #     if data.status_code == 200: 
+    #         status['Boomi'] = 'UP'
+    #     else:
+    #         status['Boomi'] = 'DOWN'
+    #     logger.info(f" health Status is {status} with status code {data.status_code} @ {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
+    # except:
+    #     
+    r
 sched = BackgroundScheduler(timezone=utc,daemon=True)  
 sched.add_job(healthy, 'interval', seconds=10)
 
